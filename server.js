45b0654b4d23ec391e6822d7b1265b60d3e3748f
Edward Nunez <edwardnunez31@gmail.com>
2021-04-19 12:45:22 -0400

    make it better

--- server.js
+++ server.js
@@ -5,21 +5,22 @@ const path = require('');
 const app = express();
 
 //Connect Database
+const app = express();
+
+// Connect Database
 connectDB();
 
-//init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {});
-
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-//Serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
@@ -29,4 +30,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
